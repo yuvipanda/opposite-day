bfa2b35c74885768e5e46d24dae06038fbb42500
YuviPanda <yuvipanda@gmail.com>
2023-08-03 21:38:05 -0700

    Put README in long description

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages, setup
 
+with open("README.md") as f:
+    readme = f.read()
+
 setup(
     name='opposite-day',
     version='1.0.0',
     description='It is Opposite Day Now',
-    url='https://github.com/jupyterhub/tmpauthenticator',
+    url='https://github.com/yuvipanda/opposite-day',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
 )
